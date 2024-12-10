80a877fffbcc9df70856342b860705beb1e9e1fd
Andrew Samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2024-12-10 13:45:41 +0200

    add: contact form

--- src/components/custom/contact-section.tsx
+++ src/components/custom/contact-section.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { GoogleMap } from "./google-map";
 import { StrapiImage } from "../strapi-image";
+import Form from "next/form";
 
 interface Link {
     id: number;
@@ -38,7 +39,7 @@ export function ContactSection({
                     <h2 className="text-3xl font-bold md:text-5xl lg:text-6xl">{title}</h2>
                     <p className="text-xl">{description}</p>
                 </div>
-                <div className="flex flex-col list-disc gap-8 ">
+                <div className="flex flex-col list-disc gap-4 ">
                     {contact.map((contact) => (
                         <div key={contact.id} className="flex items-start">
                             {contact.icon && (
@@ -70,6 +71,29 @@ export function ContactSection({
                 title={destinationTitle}
                 className="w-full h-[60vh]"
             />
+            <Form action='submit'
+                className='bg-lime-100 flex flex-col container p-4  mx-auto md:p-6 '>
+                <h2 className="text-3xl font-bold md:text-5xl lg:text-6xl">
+                    Лишились питання?
+                </h2>
+                <p>Не соромтесь, пишіть, ми тут, щоб допомогти вам із будь-якими запитами.</p>
+
+                <label htmlFor="name">Ім&apos;я</label>
+                <input name='name' type='name' className='border-amber-700 border-2' />
+
+                <label htmlFor="email">Email</label>
+                <input name='email' type='email' className='border-amber-700 border-2' />
+
+                <label htmlFor="text">{description}</label>
+                <textarea name='text' rows={4} cols={40} className='border-amber-300 border-2' />
+
+                <div>
+                    <input type="checkbox" name="ok" className="mr-2" />
+                    <label htmlFor="ok">Я погоджуюсь з умовами використання</label>
+                </div>
+
+                <button type='submit' className='m-2 p-2 border-2 border-sky-700'>Send</button>
+            </Form>
         </section>
     )
 }
--- src/components/custom/price-section.tsx
+++ src/components/custom/price-section.tsx
@@ -1,3 +1,5 @@
+import Form from 'next/form'
+
 interface PriceSectionProps {
     id: number;
     __component: string;
@@ -10,8 +12,17 @@ export function PriceSection({ data: { title, description } }: { readonly data: 
 
     return (
         <section className="flex-1 container px-4 py-6 mx-auto md:px-6 lg:py-24">
-            <h2 className="text-3xl font-bold md:text-5xl lg:text-6xl">{title}</h2>
-            <p>{description}</p>
+
+            <Form action='submit'
+                className='bg-teal-100 flex flex-col p-2 items-start gap-2'>
+                <h2 className="text-3xl font-bold md:text-5xl lg:text-6xl">{title}</h2>
+
+                <label htmlFor="email">Email</label>
+                <input name='email' type='email' className='border-amber-700 border-2'/>
+                <label htmlFor="text">{description}</label>
+                <textarea name='text' rows={4} cols={40} className='border-amber-300 border-2' />
+                <button type='submit' className='m-2 p-2 border-2 border-sky-700'>Send</button>
+            </Form>
         </section>
     )
 }
