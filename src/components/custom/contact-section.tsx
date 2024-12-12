f6abc5be0aa424da2429e0995350e824634799ba
Andrew Samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2024-12-12 12:36:41 +0200

    refactor: contact section

--- src/components/custom/contact-section.tsx
+++ src/components/custom/contact-section.tsx
@@ -31,21 +31,19 @@ interface ContactSectionProps {
 
 export function ContactSection({
     data:
-    { id, title,  description, contact } }:
+    { id, title, description, contact } }:
     { readonly data: ContactSectionProps }) {
-    // console.dir(contact);
     console.log(`!!! contact`, id);
 
     return (
-        <section className="flex-1 gap-8 container mx-auto ">
-            {/* <div className="flex gap-4"> */}
-                <div>
-                    <h2 className="text-3xl font-bold md:text-5xl lg:text-6xl">{title}</h2>
-                    <p className="text-xl">{description}</p>
-                </div>
-                <div className="flex gap-4">
-                    {contact.map((contact) => (
-                        <div key={contact.id} className="flex items-start ">
+        <section className="flex flex-col container gap-8 mx-auto ">
+            <h2 className="text-3xl font-bold md:text-5xl lg:text-6xl">{title}</h2>
+            <p className="text-xl">{description}</p>
+
+            <div className="flex flex-col gap-4">
+                {contact.map((contact) => (
+                    <div key={contact.id} className="flex flex-col">
+                        <div className="flex items-start ">
                             {contact.icon ? (
                                 <StrapiImage
                                     alt={contact.link.text}
@@ -54,64 +52,59 @@ export function ContactSection({
                                     width={24}
                                     className="mr-2"
                                 />
-                            ):''}
-
+                            ) : ''}
                             <div>
                                 <h3 className="text-lg">{contact.name}</h3>
-                                <Link
-                                    href={contact.link.url}
-                                    target="_blank"
-                                    // className="text-teal-500"
-                                >
+                                <Link href={contact.link.url} target="_blank">
                                     {contact.link.text}
                                 </Link>
-                                {contact.destination ? (
-                                    <GoogleMap
-                                        src={contact.destination}
-                                        title={contact.destinationTitle}
-                                        className=" h-[40vh]"
-                                    />
-                                ) : ''}
-          
                             </div>
                         </div>
-                    ))}
-                </div>
-            {/* </div> */}
+                        {contact.destination ? (
+                            <GoogleMap
+                                src={contact.destination}
+                                title={contact.destinationTitle}
+                                className="w-[80vw] h-[40vh]"
+                            />
+                        ) : ''}
 
-            <Form action='submit'
-                className='bg-lime-100 flex flex-col'>
-                <h2 className="text-3xl font-bold md:text-5xl lg:text-6xl">
-                    Лишились питання?
-                </h2>
-                <p>Не соромтесь, пишіть, ми тут, щоб допомогти вам із будь-якими запитами.</p>
+                    </div>
+                ))}
 
-                <label htmlFor="name">Ім&apos;я</label>
-                <input
-                    name='name'
-                    type='name'
-                    id="name"
-                    autoComplete='off'
-                    className='border-amber-700 border-2' />
+                <Form action='submit'
+                    className='bg-slate-100 flex flex-col p-4'>
+                    <h2 className="text-3xl font-bold md:text-5xl lg:text-6xl">
+                        Лишились питання?
+                    </h2>
+                    <p>Не соромтесь, пишіть, ми тут, щоб допомогти вам із будь-якими запитами.</p>
 
-                <label htmlFor="email">Email</label>
-                <input
-                    name='email'
-                    type='email'
-                    id="email"
-                    autoComplete='off'
-                    className='border-amber-700 border-2' />
+                    <label htmlFor="name">Ім&apos;я</label>
+                    <input
+                        name='name'
+                        type='name'
+                        id="name"
+                        autoComplete='off'
+                        className='border-slate-500 border-2' />
 
-                <label htmlFor="text">{description}</label>
-                <textarea name='text' id='text' rows={4} cols={40} className='border-amber-300 border-2' />
+                    <label htmlFor="email">Email</label>
+                    <input
+                        name='email'
+                        type='email'
+                        id="email"
+                        autoComplete='off'
+                        className='border-slate-500 border-2' />
 
-                <div>
-                    <input type="checkbox" name="ok" id="ok" className="mr-2" />
-                    <label htmlFor="ok">Я погоджуюсь з умовами використання</label>
-                </div>
+                    <label htmlFor="text">{description}</label>
+                    <textarea name='text' id='text' rows={4} cols={40} className='border-slate-500 border-2' />
 
-                <button type='submit' className='m-2 p-2 border-2 border-sky-700'>Send</button>
-            </Form>
+                    <div>
+                        <input type="checkbox" name="ok" id="ok" className="mr-2" />
+                        <label htmlFor="ok">Я погоджуюсь з умовами використання</label>
+                    </div>
+
+                    <button type='submit' className='m-2 p-2 border-2 border-black'>Send</button>
+                </Form>
+            </div>
         </section>
     )
 }
