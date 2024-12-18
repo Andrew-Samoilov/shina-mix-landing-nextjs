bb7dfb52455d3448bf4eac2e59da4b4d2c64dff2
Andrew Samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2024-12-18 12:27:46 +0200

    add: helper function

--- src/components/custom/footer.tsx
+++ src/components/custom/footer.tsx
@@ -1,5 +1,8 @@
 import Link from "next/link";
 import { Logo } from "@/components/custom/logo";
+import pack from '../../../package.json';
+
+const version = pack.version;
 
 interface SocialLinks {
     id: number;
@@ -21,12 +24,16 @@ interface FooterProps {
 
 export function Footer({ data }: Readonly<FooterProps>) {
     const { logoText, menu, socialLinks } = data;
+    const currentYear = new Date().getFullYear();
+    
     // console.log(`logoText`, logoText);
     // console.dir(socialLinks);
     // console.log(`menu`, menu);
+    
 
     return (
         <footer className="bg-gray-200 text-gray-600 py-8">
+
             <div className="container mx-auto px-4 md:px-6 flex flex-col md:flex-row items-center justify-between">
                 <Logo text={logoText.text} />
                 <p className="mt-4 md:mt-0 text-sm">{menu}</p>
@@ -37,7 +44,7 @@ export function Footer({ data }: Readonly<FooterProps>) {
                                 className=" hover:text-black"
                                 target="_blank" rel="noopener noreferrer"
                                 href={link.url}
-                                key={link.id+link.text}
+                                key={link.id + link.text}
                             >
                                 {link.text}
                                 <span className="sr-only">Visit us at {link.text}</span>
@@ -45,6 +52,11 @@ export function Footer({ data }: Readonly<FooterProps>) {
                         );
                     })}
                 </div>
+            </div>
+            
+            <div className="flex items-center mx-4 mt-2 px-4 md:px-6 border-t border-orange-950 justify-between" >
+                <span>©&nbsp;{currentYear}. All&nbsp;rights&nbsp;reserved.</span>
+                <span className="text-sm text-slate-400">{version}</span> 
             </div>
         </footer>
     );
--- src/components/custom/header.tsx
+++ src/components/custom/header.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { Logo } from "@/components/custom/logo";
+import { TwSizeIndicator } from "@/lib/tw-size-indicator";
 
 interface HeaderProps {
     data: {
@@ -19,14 +20,21 @@ interface HeaderProps {
 
 export async function Header({ data }: Readonly<HeaderProps>) {
     const { logoText, Menu, ctaButton } = data;
+    // console.log(`Поточне середовище: ${process.env.NODE_ENV}`);
+
+
     return (
-        <header className="flex items-center justify-between px-4 py-6 bg-white shadow-md dark:bg-gray-800">
-            <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-            <Logo text={logoText.text} />
-            <p>{Menu}</p>
-            <div className="flex items-center gap-4">
-                <Link href={ctaButton.url}>{ctaButton.text}</Link>
-            </div>
-        </header>
+        <>
+            {TwSizeIndicator()}
+            <header className="flex items-center justify-between px-4 py-6 bg-white shadow-md dark:bg-gray-800">
+
+                <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+                <Logo text={logoText.text} />
+                <p>{Menu}</p>
+                <div className="flex items-center gap-4">
+                    <Link href={ctaButton.url}>{ctaButton.text}</Link>
+                </div>
+            </header>
+        </>
     );
 }
