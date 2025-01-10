997b267d086344b39c773d395966928439e3ad9a
Andrew Samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2025-01-10 13:24:28 +0200

    refactor: sert section embla

--- src/components/custom/sert-section.tsx
+++ src/components/custom/sert-section.tsx
@@ -4,6 +4,7 @@ import { StrapiImage } from "../strapi-image";
 import Autoplay from 'embla-carousel-autoplay'
 import useEmblaCarousel from 'embla-carousel-react'
 
+import './embla.css'
 interface Image {
     id: number;
     documentId: string;
@@ -35,29 +36,25 @@ export function SertsSection({
         [Autoplay()])
 
     return (
-        <section className="container">
+        <section className="embla ">
             <h2 className="text-center">{title}</h2>
-
-            <div className="container overflow-hidden " ref={emblaRef}>
-                <div className="flex items-center space-x-6 py-6">
-
+            <div className="embla__viewport" ref={emblaRef}>
+                <div className="embla__container items-center">
                     {sert.map((sert) => (
-                        <div key={sert.id}
-                            className="flex-none w-full md:w-1/3 xl:w-1/4  md:h-1/4  items-center justify-center"
-                        >
+                        <div key={sert.id} className="embla__slide ">
                             <StrapiImage
                                 src={sert.image.url}
                                 alt={sert.image.alternativeText ?? sert.text}
                                 height={sert.image.height ?? 0}
                                 width={sert.image.width ?? 0}
-                                sizes="(min-width: 1280px) 25vw, (min-width: 768px) 33.33vw, 100vw"
+                                // sizes="(min-width: 1280px) 25vw, (min-width: 768px) 33.33vw, 100vw"
                                 className="rounded-md"
                             />
                             <div className="text-xl text-center">{sert.text}</div>
                         </div>
                     ))}
-
                 </div>
+
             </div>
 
         </section>
