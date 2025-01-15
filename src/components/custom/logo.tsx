975551e6a7ecdb04770fd51da0e93381a6fd6bfc
Andrew Samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2025-01-15 15:51:02 +0200

    chore

--- src/components/custom/logo.tsx
+++ src/components/custom/logo.tsx
@@ -1,27 +1,6 @@
 import Link from "next/link";
 import React from 'react';
 
-import theme from "../../config/theme.json";
-
-// function MountainIcon(props: Readonly<React.SVGProps<SVGSVGElement>>) {
-//     return (
-//         <svg
-//             {...props}
-//             xmlns="http://www.w3.org/2000/svg"
-//             width="24"
-//             height="24"
-//             viewBox="0 0 24 24"
-//             fill="none"
-//             stroke="currentColor"
-//             strokeWidth="2"
-//             strokeLinecap="round"
-//             strokeLinejoin="round"
-//         >
-//             <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
-//         </svg>
-//     );
-// }
-
 function SMTyreIcon(props: Readonly<React.SVGProps<SVGSVGElement>>) {
     return (
         <svg
@@ -72,7 +51,6 @@ export function Logo({
             className="flex items-center gap-2 group focus:outline-none"
             aria-label={`Navigate to ${text}`}
         >
-            {/* <MountainIcon className="h-6 w-6 text-teal-600 dark:text-teal-400 group-hover:text-teal-700 group-dark-hover:text-teal-300" /> */}
             <SMTyreIcon className="h-8 w-18 text-accent dark:text-teal-400 group-hover:text-accent group-dark-hover:text-teal-300" />
             <span className="text-2xl font-semibold group-hover:text-accent group-dark-hover:text-teal-300 -mb-3 -ml-9">
                 {text}
