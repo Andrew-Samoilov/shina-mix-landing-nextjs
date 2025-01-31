561edce3010d597620fc49e28e813c324908bf2c
Andrew Samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2025-01-31 18:02:54 +0200

    add: google analitics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,13 +6,18 @@ import { TwSizeIndicator } from "@/components/tw-size-indicator";
 import { getGlobalData } from "@/data/loaders";
 
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Шина Мікс",
   description: "Імпортер шин з власним складом в серці Києва",
   icons: {
-    icon: '/favicon.svg', 
-  }
+    icon: '/favicon.svg',
+  },
+  openGraph: {
+    type: "website",
+    locale: "uk_UA",
+  },
 };
 
 export default async function RootLayout({
@@ -24,6 +29,27 @@ export default async function RootLayout({
 
   return (
     <html lang="uk" className="scroll-smooth" >
+      <head>
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-G3EQ8VFHQT"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-G3EQ8VFHQT', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </head>
       <body className="bg-body text-base dark:bg-darkmode-body font-primary font-normal leading-relaxed text-text dark:text-darkmode-text
       flex flex-col min-h-screen">
         <TwSizeIndicator />
