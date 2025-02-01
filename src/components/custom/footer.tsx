d3d03080209cdae82c28af3fecd2a1d7c620a498
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2025-02-01 22:00:34 +0200

    refactor: sm footer style

--- src/components/custom/footer.tsx
+++ src/components/custom/footer.tsx
@@ -28,7 +28,7 @@ export function Footer({ data }: Readonly<FooterProps>) {
         <footer className="bg-theme-light dark:bg-darkmode-theme-light ">
             <div className="container mx-auto ">
                 <nav className="flex flex-col md:flex-row items-center justify-between pb-12 pt-14">
-                    <Logo text={logoText.text} />
+                    <span className="hidden md:block"><Logo text={logoText.text} /></span>
                     <div className="flex items-center space-x-6">
                         {socialLinks.map(({id, url, text}) => {
                             return (
@@ -46,16 +46,15 @@ export function Footer({ data }: Readonly<FooterProps>) {
                     </div>
                 </nav>
 
-                <div className="flex  items-center py-6 border-t border-border dark:border-darkmode-border justify-between" >
+                <div className="flex items-center py-6 border-t border-border dark:border-darkmode-border justify-between" >
                     <Link
                         href="/terms"
                         aria-label='Ознайомитись з умовами користування сайтом'
                     >
-                        Умови користування сайтом
+                        Умови<span className="hidden md:inline"> користування сайтом</span>
                     </Link>
-                    <div className="font-light ">
-                        <span>©&nbsp;{new Date().getFullYear()}. All&nbsp;rights&nbsp;reserved. </span>
-                        <span>{version}</span>
+                    <div>
+                        ©&nbsp;{new Date().getFullYear()}<span className="hidden md:inline">. All rights reserved. {version}</span>
                     </div>
                 </div>
             </div>
